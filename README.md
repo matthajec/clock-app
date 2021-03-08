ea502e2a72c66bf44dc2f0eef9d5db3444160266
matt hajec <matthew.hajec02@gmail.com>
2021-03-07 20:23:59 -0500

    made responsive

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# clock-app
- 
+# Minimalist Clock App
+This app is a minimalist clock that gives an inspiration quote from [this](https://type.fit/api/quotes) API. It also calculates and shows week of the year, day of the year, and the week number, all while adjusting for leap years.
+
+
+I used rollup.js to create a bundle for all the logic and SCSS for the CSS.
--- src/js/utils/setTime.js
+++ src/js/utils/setTime.js
@@ -18,7 +18,7 @@ export default () => {
   el('.clock_greeting').innerHTML = `<i class="fas fa-${timeOfDay === 'evening' ? 'moon' : 'sun'}"></i> Good ${timeOfDay}, it's currently`;
   el('.clock_time').textContent = `${hour > 12 ? hour - 12 : hour}:${paddedMinuteStr}`;
   el('.clock_timezone').textContent = timezone.abv;
-  el('.info-group_content[data-type="timezone"]').textContent = timezone.name.split('_').join(' '); // destroy underscores and create spaces
+  el('.info-group_content[data-type="timezone"]').textContent = timezone.name.split('_').join(' ').split('/').join(' / '); // destroy underscores and create spaces, pad "/" with spaces so that it will be split into new lines on small viewports
   el('.info-group_content[data-type="dotw"]').textContent = dayOfTheWeek;
   el('.info-group_content[data-type="doty"]').textContent = dayOfTheYear;
   el('.info-group_content[data-type="woty"]').textContent = weekOfTheYear;
